f8d8500139c948ee40c0c7edbf1b54118cb1f15d
Drew Cutchins <drew.cutch@gmail.com>
2019-11-25 16:50:26 -0800

    Update Course model with getters

--- src/models/Course.java
+++ src/models/Course.java
@@ -1,13 +1,19 @@
 package models;
 
 public class Course {
-	private int id;
+	private final int id;
+	private final String title;
+
+	public Course(int id, String title) {
+		this.id = id;
+		this.title = title;
+	}
 
 	public int getId() {
 		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
+	
+	public String getTitle() {
+		return title;
 	}
 }
