e0754e3721d40760dae463d12c370d967f649531
Michael DeLucia <mdelucia@usc.edu>
2019-11-30 13:38:37 -0800

    Mutlithreading for queue in details page

---
+++ src/db/QueueRoom.java
@@ -0,0 +1,45 @@
+package db;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class QueueRoom {
+	private Vector<Vector<ServerThread>> serverThreads;
+	public QueueRoom(int port) {
+		try {
+			System.out.println("Binding to port " + port);
+			ServerSocket ss = new ServerSocket(port);
+			System.out.println("Bound to port " + port);
+			for(int i=0; i<14; i++) {
+				Vector v = new Vector<ServerThread>();
+				serverThreads.add(v);
+			}
+			while(true) {
+				Socket s = ss.accept(); // blocking
+				System.out.println("Connection from: " + s.getInetAddress());
+				ServerThread st = new ServerThread(s, this);
+			}
+		} catch (IOException ioe) {
+			System.out.println("ioe in ChatRoom constructor: " + ioe.getMessage());
+		}
+	}
+	
+	public void broadcast(String message, ServerThread st, int courseID) {
+		if (message != null) {
+			for(ServerThread threads : serverThreads.get(courseID-1)) {
+				if (st != threads) {
+					threads.sendMessage(message);
+				}
+			}
+		}
+	}
+	
+	public void add(ServerThread st, int courseID) {
+		serverThreads.get(courseID-1).add(st);
+	}
+	
+	public static void main(String [] args) {
+		QueueRoom qr = new QueueRoom(6790);
+	}
+}
